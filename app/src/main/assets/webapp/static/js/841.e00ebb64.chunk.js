0bddc43d5023d31075897711dbfb92c7a23cc169
Carl Poole <carl@ionic.io>
2023-03-07 20:19:10 -0600

    initial commit

---
+++ app/src/main/assets/webapp/static/js/841.e00ebb64.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.e00ebb64.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkscrollytest=self.webpackChunkscrollytest||[]).push([[841],{841:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>a});var n=s(554),r=s(344),o=s(811);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,a=document.elementFromPoint(t/2,s/2);if(!a)return;const c=(0,r.a)(a);c&&new Promise((e=>(0,o.c)(c,e))).then((()=>{(0,n.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,r.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.e00ebb64.chunk.js.map
